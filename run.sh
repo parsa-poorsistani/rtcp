3ea7e179002cb63673597f11ae8976d09315239f
Parsa Poorsistani <poorsistani13@gmail.com>
2024-03-15 04:37:31 +0330

    run script modified

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 cargo build --release
 sudo setcap cap_net_admin=eip ./target/release/rtcp
-./target/release/rtcp
+./target/release/rtcp &
+pid=$!
 sudo ip addr add 192.168.0.1/24 dev tun0
 sudo ip link set up dev tun0
+wait $pid
